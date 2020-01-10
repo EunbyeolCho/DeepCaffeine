56859c3beb6ba43e5f2d2116c703b16be4d25608
parkchaesong <52907200+parkchaesong@users.noreply.github.com>
2020-01-10 15:20:13 +0900

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -54,8 +54,12 @@ def inference(opt):
         img = img.to(opt.device, dtype = torch.float)
         masks = masks.to(opt.device, dtype = torch.float)
 
-      out = net(img) #예진:unet의 out은 어떤 형식?
-
+      #out = net(img) #예진:unet의 out은 어떤 형식?
+      #채송: unet out이 모델인거같아!
+      
+      
+      
+      model = pytorch_unet.UNet(num_class).to(device)
       #target과 비교하여 평가지표 구하기 -- traget 어디에?
 
 
