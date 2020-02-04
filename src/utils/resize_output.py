5d780a567fa4c069f722aee348bded8e9d416a28
parkchaesong <52907200+parkchaesong@users.noreply.github.com>
2020-02-04 16:32:56 +0900

    Update resize_output.py

--- src/utils/resize_output.py
+++ src/utils/resize_output.py
@@ -10,15 +10,15 @@ def resize_output(opt, output, img_size):
     output = np.array(output)
     new_output = np.zeros((c, H, W))
 
-    if (H<2048) or (W<2048):
+    if (H<1536) or (W<1536):
         for i in range(c):
             new_output[i, :, :] = cv2.resize(output[i, :, :], (H,W), interpolation = cv2.INTER_CUBIC)
     else :
-        diff_h = int(round((H-2048)*0.8))
-        diff_w = int(round((W-2048)*0.5))
+        diff_h = int(round((H-1536)*0.8))
+        diff_w = int(round((W-1536)*0.5))
         # print(type(diff_h))
         # print(output.shape)
         for i in range(c):
-            new_output[i, diff_h:diff_h+2048, diff_w:diff_w+2048] = cv2.resize(output[i, :, :], (2048,2048), interpolation = cv2.INTER_CUBIC)
+            new_output[i, diff_h:diff_h+1536, diff_w:diff_w+1536] = cv2.resize(output[i, :, :], (1536,1536), interpolation = cv2.INTER_CUBIC)
 
     return new_output
